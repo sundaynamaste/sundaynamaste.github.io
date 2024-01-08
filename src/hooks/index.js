001e29a21053aff6bdc7ed1f8825c674fb3a6e52
sundaynamaste <66287913+sundaynamaste@users.noreply.github.com>
2024-01-08 11:49:33 +0900

    Delete src directory

--- src/hooks/index.js
+++
@@ -1,5 +0,0 @@
-// @flow strict
-export { default as useSiteMetadata } from './use-site-metadata';
-export { default as useCategoriesList } from './use-categories-list';
-export { default as useTagsList } from './use-tags-list';
-export { default as useLatestList } from './use-lastest-list';
